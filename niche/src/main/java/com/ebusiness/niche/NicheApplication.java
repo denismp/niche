e4f0e3a7fa194869742d52b1ad1e699c3bf5afcf
Denis Putnam <denisputnam@verizon.net>
2018-11-07 13:57:32 -0500

    Initial new files.

---
+++ niche/src/main/java/com/ebusiness/niche/NicheApplication.java
@@ -0,0 +1,12 @@
+package com.ebusiness.niche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NicheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NicheApplication.class, args);
+	}
+}
---
+++ niche/src/test/java/com/ebusiness/niche/NicheApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ebusiness.niche;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NicheApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
