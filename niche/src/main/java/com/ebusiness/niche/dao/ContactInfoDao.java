09775ee30afb05af22a470ad081aaa465c1aca65
Denis Putnam <denisputnam@verizon.net>
2019-01-05 11:05:42 -0500

    Added the ContactInfoDao and modified the query in the CompanyDao

--- niche/src/main/java/com/ebusiness/niche/dao/ContactInfoDao.java
+++ niche/src/main/java/com/ebusiness/niche/dao/ContactInfoDao.java
@@ -9,13 +9,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.ebusiness.niche.entity.Company;
+import com.ebusiness.niche.entity.ContactInfo;
 
 /**
  * @author denisputnam
  *
  */
-public interface CompanyDao extends JpaRepository<Company, Long> {
-	@Query("SELECT c FROM Company c WHERE c.email = ?1")
+public interface ContactInfoDao extends JpaRepository<ContactInfo, Long> {
+	@Query("SELECT c FROM ContactInfo c WHERE c.email = ?1")
 	public Set<Company> getByEmail( String email );
 
 }
