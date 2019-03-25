7704d7984d8c1a25757ac93643404eccd3cc0e2d
Denis Putnam <denisputnam@verizon.net>
2019-03-25 17:24:50 -0400

    Added     @CrossOrigin(origins = "http://localhost:4200") to getProducts.

--- niche/src/main/java/com/ebusiness/niche/controller/ProductController.java
+++ niche/src/main/java/com/ebusiness/niche/controller/ProductController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -30,7 +31,8 @@ public class ProductController {
 	private ProductService productService;
 	
 	@ResponseBody
-	@RequestMapping(value = {"/products"}, method = { RequestMethod.GET })
+	@RequestMapping(value = {"/product"}, method = { RequestMethod.GET })
+    @CrossOrigin(origins = "http://localhost:4200")
 	public ResponseEntity<List<Product>> getProducts() {
 		log.info("getProducts(): Called...");
 		
