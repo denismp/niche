bfab35a689e9350ca83a0116d6b0baf23d7c292f
Denis Putnam <denisputnam@verizon.net>
2019-04-02 12:25:33 -0400

    added phone and address columns to the ContactInfo entity.

--- niche/src/main/java/com/ebusiness/niche/entity/ContactInfo.java
+++ niche/src/main/java/com/ebusiness/niche/entity/ContactInfo.java
@@ -43,6 +43,31 @@ public class ContactInfo extends History {
 	private String email;
 	
 	@Column
+	private String phone1;
+	
+	@Column
+	private String phone2;
+	
+	@Column
+	private String address1;
+	
+	@Column
+	private String address2;
+	
+	@Column
+	private String city;
+	
+	@Column 
+	private String stateOrProvince;
+	
+	@Column
+	private String postalCoed;
+	
+	@Column
+	private String country;
+	
+	
+	@Column
 	@NotNull
 	private Date contactDate;
 	
@@ -91,6 +116,70 @@ public class ContactInfo extends History {
 
 	public void setProduct(Product product) {
 		this.product = product;
+	}
+
+	public String getPhone1() {
+		return phone1;
+	}
+
+	public void setPhone1(String phone1) {
+		this.phone1 = phone1;
+	}
+
+	public String getPhone2() {
+		return phone2;
+	}
+
+	public void setPhone2(String phone2) {
+		this.phone2 = phone2;
+	}
+
+	public String getAddress1() {
+		return address1;
+	}
+
+	public void setAddress1(String address1) {
+		this.address1 = address1;
+	}
+
+	public String getAddress2() {
+		return address2;
+	}
+
+	public void setAddress2(String address2) {
+		this.address2 = address2;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getStateOrProvince() {
+		return stateOrProvince;
+	}
+
+	public void setStateOrProvince(String stateOrProvince) {
+		this.stateOrProvince = stateOrProvince;
+	}
+
+	public String getPostalCoed() {
+		return postalCoed;
+	}
+
+	public void setPostalCoed(String postalCoed) {
+		this.postalCoed = postalCoed;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
 	}
 
 }
